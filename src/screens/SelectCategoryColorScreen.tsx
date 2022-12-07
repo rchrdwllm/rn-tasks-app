241a37048dcc51f28b863bc266cb47d75e11ad11
Richard William <rwilliam.flores@gmail.com>
2022-12-07 22:37:50 +0800

    optimized loading

--- src/screens/SelectCategoryColorScreen.tsx
+++ src/screens/SelectCategoryColorScreen.tsx
@@ -4,8 +4,8 @@ import { View, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Text from '../components/Text';
 import Pressable from '../components/Pressable';
-import { Shadow } from 'react-native-shadow-2';
-import { OptimizedFlatList } from 'react-native-optimized-flatlist';
+import BigList from 'react-native-big-list';
+import DownButton from '../components/DownButton';
 
 import { useNavigation } from '@react-navigation/native';
 import { colors } from '../constants/colors';
@@ -18,32 +18,19 @@ const SelectCategoryColorScreen = () => {
             onPress={() => {
                 navigation.navigate('NewCategoryScreen', { selectedColor: item });
             }}
-            twStyle="flex-1 -mt-4 mb-8"
+            twStyle="flex-row items-center bg-white py-4 px-6 space-x-3 rounded-3xl -mt-10"
         >
-            <Shadow
-                distance={20}
-                startColor={'#dbeafe37'}
-                endColor={'#ffffff00'}
-                offset={[0, 10]}
+            <View
                 style={{
-                    borderRadius: 20,
+                    height: 50,
+                    width: 50,
+                    backgroundColor: item.color,
                 }}
-                stretch
-            >
-                <View className="flex-row flex-1 bg-white rounded-2xl p-5 justify-between items-center mx-6">
-                    <Text twStyle="text-2xl mt-1" bold>
-                        {item.name}
-                    </Text>
-                    <View className="mt-2 rounded-full h-10 w-10 overflow-hidden">
-                        <View
-                            className="flex-1"
-                            style={{
-                                backgroundColor: item.color,
-                            }}
-                        ></View>
-                    </View>
-                </View>
-            </Shadow>
+                className="rounded-full"
+            ></View>
+            <View>
+                <Text>{item.name}</Text>
+            </View>
         </Pressable>
     );
 
@@ -54,21 +41,22 @@ const SelectCategoryColorScreen = () => {
                 backgroundColor: '#F9FAFF',
             }}
         >
-            <View className="flex-1">
-                <OptimizedFlatList
-                    className="flex-1"
-                    data={colors}
-                    keyExtractor={(item: Color) => item.id}
-                    ListHeaderComponent={
-                        <Text twStyle="pt-10 text-center text-xl mb-16" bold>
+            <BigList
+                data={colors}
+                keyExtractor={(item: Color) => item.id}
+                ListHeaderComponent={
+                    <View className="flex-row items-center justify-between pt-10">
+                        <Text twStyle="text-center text-xl" bold>
                             Select category color
                         </Text>
-                    }
-                    renderItem={renderFunction}
-                    initialNumToRender={10}
-                    removeClippedSubviews={true}
-                />
-            </View>
+                        <DownButton />
+                    </View>
+                }
+                renderItem={renderFunction}
+                itemHeight={98}
+                headerHeight={150}
+                contentContainerStyle={{ paddingHorizontal: 26 }}
+            />
             <StatusBar style="light" />
         </SafeAreaView>
     );

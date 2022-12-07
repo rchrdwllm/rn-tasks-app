6ae72b7bc4faca7f10899f828d636dcc391aaebe
Richard William <rwilliam.flores@gmail.com>
2022-12-07 22:36:40 +0800

    added ListEmptyComponent

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,6 +2,8 @@ import { View, FlatList } from 'react-native';
 import Text from './Text';
 import CategoryCard from './CategoryCard';
 import Pressable from './Pressable';
+import { Shadow } from 'react-native-shadow-2';
+import { PlusIcon } from 'react-native-heroicons/outline';
 
 import { useSelector } from 'react-redux';
 import { selectCategoryIds } from '../redux/slices/categoriesSlice';
@@ -47,6 +49,37 @@ const Categories = () => {
                 renderItem={renderFunction}
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ paddingLeft: 24, paddingBottom: 24 }}
+                ListEmptyComponent={
+                    <View>
+                        <Pressable
+                            onPress={() => {
+                                navigation.navigate('NewCategoryScreen');
+                            }}
+                        >
+                            <Shadow
+                                containerStyle={{
+                                    marginRight: 24,
+                                }}
+                                distance={20}
+                                startColor={'#dbeafe37'}
+                                endColor={'#ffffff00'}
+                                offset={[0, 10]}
+                                style={{
+                                    borderRadius: 20,
+                                }}
+                            >
+                                <View className={`flex-row items-center space-x-3 bg-white rounded-2xl p-5`}>
+                                    <View>
+                                        <PlusIcon size={24} color="rgb(148, 163, 184)" />
+                                    </View>
+                                    <View>
+                                        <Text twStyle="text-500 text-slate-400">Add a new category</Text>
+                                    </View>
+                                </View>
+                            </Shadow>
+                        </Pressable>
+                    </View>
+                }
             />
         </View>
     );

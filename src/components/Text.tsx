ad41e867dd47866ecf215febd847e81dd9d2bd62
Richard William <rwilliam.flores@gmail.com>
2022-12-09 00:34:57 +0800

    updated props

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,7 @@
 import type { FunctionComponent, ReactNode } from 'react';
 
 import { Text as RNText } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 import { useFonts, Rubik_400Regular, Rubik_500Medium, Rubik_600SemiBold } from '@expo-google-fonts/rubik';
 
@@ -9,10 +10,12 @@ interface TextProps {
     style?: {};
     bold?: boolean;
     medium?: boolean;
+    animated?: boolean;
+    animatedStyle?: {};
     children: ReactNode;
 }
 
-const Text: FunctionComponent<TextProps> = ({ twStyle, style, bold, medium, children }) => {
+const Text: FunctionComponent<TextProps> = ({ twStyle, style, bold, medium, children, animated, animatedStyle }) => {
     const [fontsLoaded] = useFonts({
         Rubik: Rubik_400Regular,
         RubikMedium: Rubik_500Medium,
@@ -20,6 +23,22 @@ const Text: FunctionComponent<TextProps> = ({ twStyle, style, bold, medium, chil
     });
 
     if (!fontsLoaded) return null;
+
+    if (animated)
+        return (
+            <Animated.Text
+                className={`text-slate-800 ${twStyle}`}
+                style={[
+                    {
+                        fontFamily: bold ? 'RubikBold' : medium ? 'RubikMedium' : 'Rubik',
+                        ...style,
+                    },
+                    animatedStyle,
+                ]}
+            >
+                {children}
+            </Animated.Text>
+        );
 
     return (
         <RNText

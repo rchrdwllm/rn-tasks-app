8da054cea0919d9ff57668910c844f224148823c
Richard William <rwilliam.flores@gmail.com>
2022-12-21 15:45:10 +0800

    added cloudinary

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,11 @@
+import { v2 } from 'cloudinary';
+
+const cloudinary = v2;
+
+cloudinary.config({
+    cloud_name: 'rwilliam',
+    api_key: '677182174977128',
+    api_secret: 'XAcoEtXVfXdszd2K4s_To0xdBfE',
+});
+
+export default cloudinary;
